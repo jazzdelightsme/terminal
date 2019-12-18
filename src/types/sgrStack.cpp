8b4cccfa73d8ef9c7e9f81c75c7089b5ab999263
Dan Thompson (SBS) <danthom@microsoft.com>
2019-12-17 23:24:36 -0800

    formatting fix

--- src/types/sgrStack.cpp
+++ src/types/sgrStack.cpp
@@ -6,209 +6,208 @@
 
 namespace Microsoft::Console::VirtualTerminal
 {
-
-SgrStack::SgrStack() noexcept :
-    _nextPushIndex{ 0 },
-    _numSavedAttrs{ 0 }
-{
-}
-
-void SgrStack::Push(const TextAttribute& currentAttributes,
-                    const gsl::span<const DispatchTypes::SgrSaveRestoreStackOptions> options) noexcept
-{
-    AttrBitset validParts;
-
-    if (options.size() == 0)
+    SgrStack::SgrStack() noexcept :
+        _nextPushIndex{ 0 },
+        _numSavedAttrs{ 0 }
     {
-        // We save all current attributes.
-        validParts.set(); // (sets all bits)
     }
-    else
-    {
-        // Each option is encoded as a bit in validParts. Options that aren't
-        // supported are ignored. So if you try to save only unsuppported aspects
-        // of the current text attributes, validParts end up as zero, and you'll do
-        // what is effectively an "empty" push (the subsequent pop will not change
-        // the current attributes), which is the correct behavior.
-        for (auto option : options)
-        {
-            size_t optionAsIndex = static_cast<size_t>(option);
 
-            // Options must be specified singly; not in combination. Values that are
-            // out of range will be ignored.
-            if (optionAsIndex < validParts.size())
+    void SgrStack::Push(const TextAttribute& currentAttributes,
+                        const gsl::span<const DispatchTypes::SgrSaveRestoreStackOptions> options) noexcept
+    {
+        AttrBitset validParts;
+
+        if (options.size() == 0)
+        {
+            // We save all current attributes.
+            validParts.set(); // (sets all bits)
+        }
+        else
+        {
+            // Each option is encoded as a bit in validParts. Options that aren't
+            // supported are ignored. So if you try to save only unsuppported aspects of
+            // the current text attributes, validParts end up as zero, and you'll do what
+            // is effectively an "empty" push (the subsequent pop will not change the
+            // current attributes), which is the correct behavior.
+            for (auto option : options)
             {
-                validParts.set(optionAsIndex);
+                size_t optionAsIndex = static_cast<size_t>(option);
+
+                // Options must be specified singly; not in combination. Values that are
+                // out of range will be ignored.
+                if (optionAsIndex < validParts.size())
+                {
+                    validParts.set(optionAsIndex);
+                }
             }
         }
-    }
 
-    if (_numSavedAttrs < _storedSgrAttributes.size())
-    {
-        _numSavedAttrs++;
-    }
-
-    _storedSgrAttributes[_nextPushIndex] = { currentAttributes, validParts };
-    _nextPushIndex = (_nextPushIndex + 1) % _storedSgrAttributes.size();
-}
-
-const TextAttribute SgrStack::Pop(const TextAttribute& currentAttributes) noexcept
-{
-    if (_numSavedAttrs > 0)
-    {
-        _numSavedAttrs--;
-
-        if (_nextPushIndex == 0)
+        if (_numSavedAttrs < _storedSgrAttributes.size())
         {
-            _nextPushIndex = gsl::narrow<int>(_storedSgrAttributes.size() - 1);
-        }
-        else
-        {
-            _nextPushIndex--;
+            _numSavedAttrs++;
         }
 
-        SavedSgrAttributes& restoreMe = _storedSgrAttributes[_nextPushIndex];
-
-        if (restoreMe.ValidParts.all())
-        {
-            return restoreMe.TextAttributes;
-        }
-        else
-        {
-            return _CombineWithCurrentAttributes(currentAttributes,
-                                                 restoreMe.TextAttributes,
-                                                 restoreMe.ValidParts);
-        }
+        _storedSgrAttributes[_nextPushIndex] = { currentAttributes, validParts };
+        _nextPushIndex = (_nextPushIndex + 1) % _storedSgrAttributes.size();
     }
 
-    return currentAttributes;
-}
-
-TextAttribute SgrStack::_CombineWithCurrentAttributes(const TextAttribute& currentAttributes,
-                                                      const TextAttribute& savedAttribute,
-                                                      const AttrBitset validParts) noexcept // of savedAttribute
-{
-    TextAttribute result = currentAttributes;
-
-    // From xterm documentation:
-    //
-    //  CSI # {
-    //  CSI Ps ; Ps # {
-    //            Push video attributes onto stack (XTPUSHSGR), xterm.  The
-    //            optional parameters correspond to the SGR encoding for video
-    //            attributes, except for colors (which do not have a unique SGR
-    //            code):
-    //              Ps = 1  -> Bold.
-    //              Ps = 2  -> Faint.
-    //              Ps = 3  -> Italicized.
-    //              Ps = 4  -> Underlined.
-    //              Ps = 5  -> Blink.
-    //              Ps = 7  -> Inverse.
-    //              Ps = 8  -> Invisible.
-    //              Ps = 9  -> Crossed-out characters.
-    //              Ps = 1 0  -> Foreground color.
-    //              Ps = 1 1  -> Background color.
-    //              Ps = 2 1  -> Doubly-underlined.
-    //
-    //  (some closing braces for people with editors that get thrown off without them: }})
-    //
-    // Note that not all of these attributes are actually supported by renderers/conhost,
-    // despite setters/getters on TextAttribute.
-
-    // Boldness = 1,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Boldness)))
+    const TextAttribute SgrStack::Pop(const TextAttribute& currentAttributes) noexcept
     {
-        if (savedAttribute.IsBold())
+        if (_numSavedAttrs > 0)
         {
-            result.Embolden();
-        }
-        else
-        {
-            result.Debolden();
-        }
-    }
+            _numSavedAttrs--;
 
-    // Faintness = 2,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Faintness)))
-    {
-        result.SetFaint(savedAttribute.IsFaint());
-    }
-
-    // Italics = 3,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Italics)))
-    {
-        result.SetItalics(savedAttribute.IsItalicized());
-    }
-
-    // Underline = 4,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Underline)))
-    {
-        if (savedAttribute.IsUnderline())
-        {
-            result.EnableUnderline();
-        }
-        else
-        {
-            result.DisableUnderline();
-        }
-    }
-
-    // Blink = 5,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Blink)))
-    {
-        result.SetBlinking(savedAttribute.IsBlinking());
-    }
-
-    // Negative = 7,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Negative)))
-    {
-        if (savedAttribute.IsReverseVideo())
-        {
-            if (!result.IsReverseVideo())
+            if (_nextPushIndex == 0)
             {
-                result.Invert();
+                _nextPushIndex = gsl::narrow<int>(_storedSgrAttributes.size() - 1);
+            }
+            else
+            {
+                _nextPushIndex--;
+            }
+
+            SavedSgrAttributes& restoreMe = _storedSgrAttributes[_nextPushIndex];
+
+            if (restoreMe.ValidParts.all())
+            {
+                return restoreMe.TextAttributes;
+            }
+            else
+            {
+                return _CombineWithCurrentAttributes(currentAttributes,
+                                                     restoreMe.TextAttributes,
+                                                     restoreMe.ValidParts);
             }
         }
-        else
+
+        return currentAttributes;
+    }
+
+    TextAttribute SgrStack::_CombineWithCurrentAttributes(const TextAttribute& currentAttributes,
+                                                          const TextAttribute& savedAttribute,
+                                                          const AttrBitset validParts) noexcept // of savedAttribute
+    {
+        TextAttribute result = currentAttributes;
+
+        // From xterm documentation:
+        //
+        //  CSI # {
+        //  CSI Ps ; Ps # {
+        //            Push video attributes onto stack (XTPUSHSGR), xterm.  The
+        //            optional parameters correspond to the SGR encoding for video
+        //            attributes, except for colors (which do not have a unique SGR
+        //            code):
+        //              Ps = 1  -> Bold.
+        //              Ps = 2  -> Faint.
+        //              Ps = 3  -> Italicized.
+        //              Ps = 4  -> Underlined.
+        //              Ps = 5  -> Blink.
+        //              Ps = 7  -> Inverse.
+        //              Ps = 8  -> Invisible.
+        //              Ps = 9  -> Crossed-out characters.
+        //              Ps = 1 0  -> Foreground color.
+        //              Ps = 1 1  -> Background color.
+        //              Ps = 2 1  -> Doubly-underlined.
+        //
+        //  (some closing braces for people with editors that get thrown off without them: }})
+        //
+        // Note that not all of these attributes are actually supported by
+        // renderers/conhost, despite setters/getters on TextAttribute.
+
+        // Boldness = 1,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Boldness)))
         {
-            if (result.IsReverseVideo())
+            if (savedAttribute.IsBold())
             {
-                result.Invert();
+                result.Embolden();
+            }
+            else
+            {
+                result.Debolden();
             }
         }
-    }
 
-    // Invisible = 8,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Invisible)))
-    {
-        result.SetInvisible(savedAttribute.IsInvisible());
-    }
+        // Faintness = 2,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Faintness)))
+        {
+            result.SetFaint(savedAttribute.IsFaint());
+        }
 
-    // CrossedOut = 9,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::CrossedOut)))
-    {
-        result.SetCrossedOut(savedAttribute.IsCrossedOut());
-    }
+        // Italics = 3,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Italics)))
+        {
+            result.SetItalics(savedAttribute.IsItalicized());
+        }
 
-    // SaveForegroundColor = 10,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::SaveForegroundColor)))
-    {
-        result.SetForegroundFrom(savedAttribute);
-    }
+        // Underline = 4,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Underline)))
+        {
+            if (savedAttribute.IsUnderline())
+            {
+                result.EnableUnderline();
+            }
+            else
+            {
+                result.DisableUnderline();
+            }
+        }
 
-    // SaveBackgroundColor = 11,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::SaveBackgroundColor)))
-    {
-        result.SetBackgroundFrom(savedAttribute);
-    }
+        // Blink = 5,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Blink)))
+        {
+            result.SetBlinking(savedAttribute.IsBlinking());
+        }
 
-    // DoublyUnderlined = 21,
-    if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::DoublyUnderlined)))
-    {
-        result.SetDoublyUnderlined(savedAttribute.IsDoublyUnderlined());
-    }
+        // Negative = 7,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Negative)))
+        {
+            if (savedAttribute.IsReverseVideo())
+            {
+                if (!result.IsReverseVideo())
+                {
+                    result.Invert();
+                }
+            }
+            else
+            {
+                if (result.IsReverseVideo())
+                {
+                    result.Invert();
+                }
+            }
+        }
 
-    return result;
-}
+        // Invisible = 8,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::Invisible)))
+        {
+            result.SetInvisible(savedAttribute.IsInvisible());
+        }
+
+        // CrossedOut = 9,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::CrossedOut)))
+        {
+            result.SetCrossedOut(savedAttribute.IsCrossedOut());
+        }
+
+        // SaveForegroundColor = 10,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::SaveForegroundColor)))
+        {
+            result.SetForegroundFrom(savedAttribute);
+        }
+
+        // SaveBackgroundColor = 11,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::SaveBackgroundColor)))
+        {
+            result.SetBackgroundFrom(savedAttribute);
+        }
+
+        // DoublyUnderlined = 21,
+        if (validParts.test(static_cast<size_t>(DispatchTypes::SgrSaveRestoreStackOptions::DoublyUnderlined)))
+        {
+            result.SetDoublyUnderlined(savedAttribute.IsDoublyUnderlined());
+        }
+
+        return result;
+    }
 
 }
